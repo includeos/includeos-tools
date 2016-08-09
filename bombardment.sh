5ad7da30281a52587436f667ec1f631aeb393618
Martin Nordsletten <mnordsletten@gmail.com>
2016-08-09 13:24:58 +0000

    bombardment: Moved out check if up function

--- bombardment.sh
+++ bombardment.sh
@@ -13,9 +13,8 @@ BURST_SIZE=1000
 
 
 # Initiate tests through the stress-clients
-IP_IOS_SERVER=10.10.10.132
-IP_STRESS_1=10.10.10.131 
-IP_STRESS_2=10.10.10.133
+IP_HOST=$1
+IP_IOS_SERVER=$2
 
 function ping_flood {
     IP_HOST=$1
@@ -49,38 +48,6 @@ function arping_flood {
         '
     }
 
-
-function check_if_up {
-    IP_HOST=$1
-    IP_TARGET=$2
-
-    ssh $IP_HOST -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-        'ping '"'$IP_TARGET'"' -c 1 > /dev/null 2>&1
-        '
-    if [ $? -ne 0 ] 
-        then
-        echo "Ping did not receive a reply, exiting"
-        exit 1
-    fi
-
-
-    ssh $IP_HOST -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-        'wget '"'$IP_TARGET'"' > /dev/null 2>&1
-        '
-    if [ $? -ne 0 ]
-        then
-        echo "Wget did not download correctly, exiting"
-        exit 1
-    fi
-    }
-
-for HOST in $IP_STRESS_1 $IP_STRESS_2
-do
-    ping_flood $HOST $IP_IOS_SERVER
-    httperf_flood $HOST $IP_IOS_SERVER
-    arping_flood $HOST $IP_IOS_SERVER
-
-    check_if_up $HOST $IP_IOS_SERVER
-    echo "It's up"
-done
-
+ping_flood $IP_HOST $IP_IOS_SERVER
+httperf_flood $IP_HOST $IP_IOS_SERVER
+arping_flood $IP_HOST $IP_IOS_SERVER
