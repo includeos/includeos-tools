b00d9a28044fd089570ec0b3fb5e41b87e82dff2
Martin Nordsletten <mnordsletten@gmail.com>
2016-08-17 13:18:19 +0000

    bombardment: Now process all output from httperf

--- bombardment.py
+++ bombardment.py
@@ -5,11 +5,11 @@ import time
 import re
 import argparse
 import commands
-from multiprocessing import Process
+from multiprocessing import Process, Pool, Queue
 
 RATE = 700
 BURST_SIZE = RATE * 10
-HOST = "10.10.10.142"
+HOST = "10.10.10.147"
 STRESS_HOST_1 = "10.10.10.131"
 STRESS_HOST_2 = "10.10.10.133"
 SSH_HOST = [STRESS_HOST_1, STRESS_HOST_2]
@@ -22,13 +22,11 @@ def ICMP_flood(burst_size=BURST_SIZE):
 
 
 # Fire a single burst of HTTP requests
-def httperf(ssh_target=STRESS_HOST_1, burst_size=BURST_SIZE, rate=RATE, target=HOST):
-    print "in httperf"
+def httperf(q, ssh_target=STRESS_HOST_1, burst_size=BURST_SIZE, rate=RATE, target=HOST):
     command = "ssh {0} -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ' \
                httperf --hog --server {1} --num-conn {2} --rate {3} ' \
                ".format(ssh_target, target, str(burst_size), str(rate))
     res = commands.getstatusoutput(command)[1]  # Returns list, we want item 1
-    print "command done"
     regex = ("requests (?P<tot_requests>\S*) replies (?P<tot_replies>\S*) test(.|\n)*"
              "Connection rate: (?P<conn_rate>\S*) conn/s (.|\n)*"
              ".*replies/s]: min \S* avg (?P<reply_rate_avg>\S*) max")
@@ -40,8 +38,16 @@ def httperf(ssh_target=STRESS_HOST_1, burst_size=BURST_SIZE, rate=RATE, target=H
         "reply_rate_avg": output.group("reply_rate_avg")
     }
 
-    print results
-    return results
+    q.put(results)
+    return
+
+def cleanup(ssh_target):
+    """Will make sure no httperf processes are running on the ssh_host"""
+
+    command = "ssh {0} -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ' \
+               pkill httperf '".format(ssh_target)
+    commands.getstatusoutput(command)
+    return
 
 
 # Fire a single burst of ARP requests
@@ -51,18 +57,66 @@ def ARP_burst(burst_size = BURST_SIZE):
     res = subprocess.check_output(command)
     return res
 
+def print_results(res):
+    """Print the results in a nicely formatted way"""
+    print ("Total requests: {0} \t"
+           "Total replies: {1} \t"
+           "Connection rate: {2} \t"
+           "Reply rate avg: {3}".format(res['total_requests'],
+                                        res['total_replies'],
+                                        res['connection_rate'],
+                                        res['reply_rate_avg']))
 
 def main():
 
     parser = argparse.ArgumentParser(description="Start a bombardment")
     parser.add_argument("-r", "--rate", default="200", dest="rate", help="http packets pr. second to send")
     parser.add_argument("-b", "--burst_size", default="100000", dest="burst_size", help="Total number of packets to send")
+    parser.add_argument("-l", "--loops", default=1, type=int, dest="loops", help="Number of loops to perform")
     args = parser.parse_args()
 
-    for ip in SSH_HOST:
-        print ip
-        Process(target=httperf, args=(ip, args.burst_size, args.rate)).start()
+    q = Queue()
+    p = {}
+    total_results = {'total_number_of_loops': 0,
+                     'multiplier': 0,
+                     'number_of_ssh_hosts': float(len(SSH_HOST)),
+                     'total_connection_rate': 0,
+                     'connection_rate': 0,
+                     'reply_rate_avg': 0,
+                     'total_replies': 0,
+                     'total_requests': 0}
+    while(args.loops > 0):
+        for ip in SSH_HOST:
+            p[ip] = Process(target=httperf, args=(q, ip, args.burst_size, args.rate))
+            p[ip].start()
 
+        for ip in SSH_HOST:
+            timeout_value = float(args.burst_size) / float(args.rate) * 1.1
+            p[ip].join(timeout_value)
+            print "Now joining {0}".format(ip)
+            if p[ip].is_alive():
+                p[ip].terminate()
+                print "terminated"
+                continue
+            results = q.get()
+
+            # Calculating loop
+            total_results['total_number_of_loops'] += 1
+            total_results['multiplier'] = total_results['total_number_of_loops'] / float(total_results['number_of_ssh_hosts'])
+
+            # Connection rate calculations
+            total_results['total_connection_rate'] += float(results['connection_rate'])
+            total_results['connection_rate'] = total_results['total_connection_rate'] / total_results['multiplier']
+
+            total_results['reply_rate_avg'] = float(results['reply_rate_avg'])
+            total_results['total_replies'] += int(results['total_replies'])
+            total_results['total_requests'] += int(results['total_requests'])
+        args.loops -= 1
+        print "Loop {0} results:".format(args.loops)
+        print_results(total_results)
+
+    for ip in SSH_HOST:
+        cleanup(ip)
 
 
 if __name__ == "__main__":
