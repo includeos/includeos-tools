adcb62c8f2f60e9718961bd2079e7a1363cd996b
Martin Nordsletten <mnordsletten@gmail.com>
2016-08-25 08:02:33 +0000

    Removed some unused scripts

--- prepare_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install necessary packages for running the IncludeOS tests
-
-sudo echo 127.0.0.1 $HOSTNAME >> /etc/hosts
-
-sudo apt-get update
-sudo apt-get install -y httperf arping python-pip
-sudo pip install jsonschema
