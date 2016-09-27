60bf24f660c3494f3809ce7426c0fe3765aabde6
Martin Nordsletten <mnordsletten@gmail.com>
2016-09-27 12:55:47 +0000

    puppet: Added puppet configuration for a test client

---
+++ puppet/test_client.pp
@@ -0,0 +1,21 @@
+package { "httperf" :
+        ensure => present,
+}
+package { "arping" :
+        ensure => present,
+}
+package { "python-jsonschema" :
+        ensure => present,
+}
+package { "dnsmasq" :
+        ensure => present,
+}
+service { "dnsmasq" :
+        ensure => running,
+        require => Package['dnsmasq'],
+}
+exec { "modify-dnsmasq" :
+        path => "/opt/puppetlabs/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
+        command => 'echo -e "interface=include0 \ndhcp-range=10.0.0.2,10.0.0.200,12h" >> /etc/dnsmasq.conf',
+        unless => 'grep -q include0 /etc/dnsmasq.conf',
+}
