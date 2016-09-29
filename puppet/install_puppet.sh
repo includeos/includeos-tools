6c0e429eb6a4490903a5bdb528af6b400957d42f
Martin Nordsletten <mnordsletten@gmail.com>
2016-09-29 11:55:40 +0000

    puppet: Now installs correctly

--- puppet/install_puppet.sh
+++ puppet/install_puppet.sh
@@ -5,5 +5,6 @@ sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt-get update
 
 sudo apt-get install puppet-agent
+sudo apt-get install gcc-5 g++-5
 
-/opt/puppetlabs/bin/puppet agent ~/includeos-tools/puppet/test_client.pp
+sudo /opt/puppetlabs/bin/puppet apply ~/includeos-tools/puppet/test_client.pp
