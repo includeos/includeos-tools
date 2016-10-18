97833b89e7c5ca650c9a7fea0512dfd4e58cf8e5
Martin Nordsletten <mnordsletten@gmail.com>
2016-10-18 10:58:23 +0200

    "Bombardment: Added parsing of httperf output"

--- bombardment/perf_stats.py
+++ bombardment/perf_stats.py
@@ -5,6 +5,7 @@ import re
 import os
 import sys
 import argparse
+import signal
 
 # Module import of openstack control script
 """
@@ -13,6 +14,11 @@ __location__ = os.path.realpath(
 sys.path.append('../openstack_control')
 import openstack_control
 """
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 
 class httperf():
@@ -31,6 +37,10 @@ class httperf():
 
         self.client = client
         self.target = target
+        self.tot_requests = 0
+        self.tot_replies = 0
+        self.conn_rate = 0
+        self.reply_rate_avg = 0
 
     def __str__(self):
         """str function used for printing"""
@@ -38,6 +48,10 @@ class httperf():
                 'Target: {x[target]}\n'
                 'Rate: {x[rate]}\n'
                 'Num_conns: {x[num_conns]}\n'
+                'tot_requests: {x[tot_requests]}\n'
+                'tot_replies: {x[tot_replies]}\n'
+                'conn_rate: {x[conn_rate]}\n'
+                'reply_rate_avg: {x[reply_rate_avg]}\n'
                 ).format(x=self.__dict__)
 
     def run(self, rate=500, num_conns=5000):
@@ -54,6 +68,21 @@ class httperf():
 
         # Run the command
         result = subprocess.check_output(command, shell=True)
+        
+        # Process output
+        regex = ("requests (?P<tot_requests>\S*) replies (?P<tot_replies>\S*) test(.|\n)*"
+                 "Connection rate: (?P<conn_rate>\S*) conn/s (.|\n)*"
+                 ".*replies/s]: min \S* avg (?P<reply_rate_avg>\S*) max")
+        output = re.search(regex, res)
+        
+        self.tot_requests = output.group('tot_requests')
+        self.tot_replies = output.group('tot_replies')
+        self.conn_rate = output.group('conn_rate')
+        self.reply_rate_avg = output.group('reply_rate_avg')
+        
+        return
+        
+        
 
 
 
