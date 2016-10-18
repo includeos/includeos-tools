afeae017475bd5b33b16f6d476ce42a64ae09fc0
Martin Nordsletten <mnordsletten@gmail.com>
2016-10-18 08:20:46 +0000

    bombardment: Now runs httperf over ssh

--- bombardment/perf_stats.py
+++ bombardment/perf_stats.py
@@ -5,7 +5,6 @@ import re
 import os
 import sys
 import argparse
-from multiprocessing import Process, Queue
 
 # Module import of openstack control script
 """
@@ -19,20 +18,20 @@ import openstack_control
 class httperf():
     """ Contains all httperf related functions
     """
-    
+
     def __init__(self, client, target):
         """Initialize arguments
-        
+
         Keyword arguments:
         client      -- IP to the client where httperf runs
         target      -- IP to the target getting tested
         rate        -- Number of connections pr. second (default 500)
         num_conns   -- Total number of connections (default 5000)
         """
-        
+
         self.client = client
         self.target = target
-        
+
     def __str__(self):
         """str function used for printing"""
         return ('Client: {x[client]}\n'
@@ -40,31 +39,31 @@ class httperf():
                 'Rate: {x[rate]}\n'
                 'Num_conns: {x[num_conns]}\n'
                 ).format(x=self.__dict__)
-    
+
     def run(self, rate=500, num_conns=5000):
         """Starts a run of the httperf command
-        
+
         Keyword arguments:
         rate        -- Number of connections pr. second (default 500)
         num_conns   -- Total number of connections (default 5000)
         """
-        
-        command = ('ssh {0} -q -o StrictKeyChecking=no -o UserKnownHostsFile=/dev/null '
-                   '"httperf --hog --server {1} --num_conn {2} --rate {3}" '
+
+        command = ('ssh {0} -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null '
+                   '"httperf --hog --server {1} --num-conns {2} --rate {3}" '
                    ).format(self.client, self.target, num_conns, rate)
-        
-        # Run the command            
-        result = subprocess.check_output(command)
-        print result
-        
-                  
-        
-        
+
+        # Run the command
+        result = subprocess.check_output(command, shell=True)
+
+
+
+
+
 
 if __name__ == '__main__':
-    client = '10.10.10.10'
-    target = '11.11.11.11'
-    rate = 1000
-    num_conns = 5000
+    client = '10.10.10.133'
+    target = '10.10.10.124'
+    rate = 100
+    num_conns = 500
     obj = httperf(client, target)
     obj.run(rate, num_conns)
