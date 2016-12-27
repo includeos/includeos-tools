7d5209e0ac8033035d568030e9dde96b9d516b39
Martin Nordsletten <mnordsletten@gmail.com>
2016-12-27 12:10:46 +0000

    Executable and -y for install

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -4,12 +4,12 @@
 # Taken from: https://docs.docker.com/engine/installation/linux/ubuntulinux/
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo deb https://apt.dockerproject.org/repo ubuntu-xenial main | sudo tee /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get install docker-engine
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y docker-engine
 
 sudo service docker start
