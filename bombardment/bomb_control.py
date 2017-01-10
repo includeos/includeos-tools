22c340528f3278861537cd123acfb8168a4076cd
Martin Nordsletten <mnordsletten@gmail.com>
2017-01-10 14:46:48 +0100

    bombardment: Restart and status functions

--- bombardment/bomb_control.py
+++ bombardment/bomb_control.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python
 import perf_stats as perfs
+import sys
+import urllib2
 
-def increase_intensity(client):
+sys.path.insert(0, "../openstack_control")
+import openstack_control as ostack
+
+def target_status(target):
+    """ Finds the status of the target"""
+
+    try:
+        return_code = urllib2.urlopen('http://'+target, timeout=2).getcode()
+        return True
+    except urllib2.URLError:
+        # Timed out
+        return False
+
+def target_restart():
+    """ Will restart the target """
+    acorn_image = "acorn2012"
+    # Stop previous target
+    ostack.vm_stop("bomb_target")
+
+    # Remove previous target
+    ostack.vm_delete("bomb_target")
+
+    # Start new target
+    ostack.vm_create("bomb_target", image=acorn_image, flavor="includeos.micro", network="Private_IncludeOS", key_pair="IncludeOS")
+
+    # Return new IP
+    return ostack.vm_status("bomb_target")['network'][1]
+
+
+
+def find_breaking_point(client, target):
     """ Will increase the intensity of the test
     """
 
-    obj = perfs.Httperf(client, '10.10.10.135')
+    obj = perfs.Httperf(client, target)
 
     rate = 1500
-    for test in range(5):
-        rate += 100
-        num_conns = rate * 2
-        obj.run(rate, num_conns)
-        x = [obj]
-        print perfs.statcalc(x)
+    for test in range(10):
+        if target_status(target):
+            print "Running test ", test
+            rate += 200
+            num_conns = rate * 2
+            obj.run(rate, num_conns)
+            x = [obj]
+            print perfs.statcalc(x)
 
 def main():
     """ Main function
@@ -21,7 +55,7 @@ def main():
 
     client = '10.10.10.132'
     client2 = '10.10.10.134'
-    target = '10.10.10.135'
+    target = ostack.vm_status('bomb_target')['network'][1]
     rate = 200
     num_conns = 2000
     obj = perfs.Httperf(client, target)
@@ -33,7 +67,7 @@ def main():
     print perfs.statcalc(x)
     """
 
-    increase_intensity(client)
+    find_breaking_point(client, target)
 
 
 
