02d8c80ec71737bf07acf902c63ad162cf4167e9
Martin Nordsletten <mnordsletten@gmail.com>
2017-03-09 10:34:59 +0100

    test: Added openstack intrusive test

---
+++ tests/test_intrusive_openstack.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -x
+
+echo -e ">>> Running Intrusive Test"
+
+INCLUDEOS_SRC=${INCLUDEOS_SRC-~/IncludeOS}
+INCLUDEOS_TOOLS=${INCLUDEOS_TOOLS-~/includeos-tools}
+NAME=pull_request_intrusive
+IMAGE_NAME=intrusive-snapshot
+KEY_PAIR_NAME="pr_openstack"
+
+# Preemptive checks to see if there is openstack support
+echo -e "\n\n>>> Checking if the required Openstack tools are installed"
+errors=0
+dpkg -l | grep nova > /dev/null 2>&1 || { echo "Nova is required"; errors=$((errors + 1)); }; 
+if [ ! -f $INCLUDEOS_TOOLS/openstack_control/openstack_control.py ]; then
+	echo "openstack_control.py is required"
+   	errors=$((errors + 1))  
+fi
+if [ $errors -gt 0 ]; then
+	echo You do not have the required programs for running the Openstack test, Exiting
+	exit 1
+fi
+
+# Create trap to ensure clean up
+function clean {
+	echo -e "\n\n>>> Performing clean up"
+	$INCLUDEOS_TOOLS/openstack_control/openstack_control.py --delete $NAME
+	echo $errors
+}
+trap clean EXIT
+
+echo Starting instance
+IP="$($INCLUDEOS_TOOLS/openstack_control/openstack_control.py --create $NAME --flavor g1.small --image $IMAGE_NAME --key_pair "$KEY_PAIR_NAME")"
+echo Instance started on IP: $IP
+
+timeout=0
+until ssh -q $IP exit || [ "$timeout" -gt 30 ]
+do
+	sleep 1
+	timeout=$((timeout+1))
+done
+
+
+ssh $IP 'mkdir workspace; cd workspace
+		 wget -q 10.10.10.125:8080/built.tar.gz
+		 tar -zxf built.tar.gz
+		 cd test
+		 ./testrunner.py -t intrusive'
