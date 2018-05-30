52e166cd6816ee2b64bcbd1bb6aba25fb6fd6d95
Syeda Taiyeba Haroon <staiyeba@sth-includeosx.local>
2018-05-30 15:31:59 +0200

    puppet: adding checkinstall instead of make install

--- puppet/install_gcc_httperf.sh
+++ puppet/install_gcc_httperf.sh
@@ -7,7 +7,8 @@ sudo apt-get install -y gcc-7 g++-7
 
 # installing httperf from source
 
-sudo apt-get install autoconf
+sudo apt-get install -y autoconf
+sudo apt-get install -y libtool
 wget https://github.com/rtCamp/httperf/archive/master.zip
 sudo apt-get install -y unzip
 unzip master.zip
@@ -17,8 +18,8 @@ mkdir build
 cd build
 ../configure
 make
-make install
-
+#sudo make install
+sudo checkinstall # using checkinstall eases installation and uninstallation apps compiled from source.
 
 # this output shows max no. of open descriptors
 httperf -v | grep open
