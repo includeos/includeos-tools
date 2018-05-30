1896458355cacc5a2a1665dbfdd554fc5cbb10b2
Syeda Taiyeba Haroon <staiyeba@sth-includeosx.local>
2018-05-30 12:45:04 +0200

    puppet: aupdating script - install httperf and gcc

--- puppet/install_gcc_httperf.sh
+++ puppet/install_gcc_httperf.sh
@@ -1,14 +1,15 @@
 
 # installing gcc eversion 7.1 as needed
 
-sudo add-apt-repository ppa:jonathonf/gcc-7.1
+sudo add-apt-repository -y ppa:jonathonf/gcc-7.1
 sudo apt-get update
-sudo apt-get install gcc-7 g++-7
+sudo apt-get install -y gcc-7 g++-7
 
 # installing httperf from source
 
 sudo apt-get install autoconf
 wget https://github.com/rtCamp/httperf/archive/master.zip
+sudo apt-get install -y unzip
 unzip master.zip
 cd httperf-master
 autoreconf -i
