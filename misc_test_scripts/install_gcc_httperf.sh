1716683062f1dca727493f6b4fd855989a5564fb
taiyeba <staiyeba@gmail.com>
2018-06-11 10:38:51 +0200

    misc_test: updating with sudo

--- misc_test_scripts/install_gcc_httperf.sh
+++ misc_test_scripts/install_gcc_httperf.sh
@@ -9,7 +9,7 @@ echo "export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8">>~/.bash_profile
 
-# locale-gen en_US.UTF-8
+# sudo locale-gen en_US.UTF-8
 
 # sudo dpkg-reconfigure -u locales
 
