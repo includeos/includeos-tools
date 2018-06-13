f5a8b5b69388c93b6e4fe8c9cf9fffd5e4f20bb4
taiyeba <staiyeba@gmail.com>
2018-06-13 10:30:21 +0200

    puppet: setting locales before puppet install

--- puppet/install_puppet_and_test_client.sh
+++ puppet/install_puppet_and_test_client.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Setting locale..."
+echo "export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8">>~/.bash_profile
+source ~/.bash_profile
+
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
 sudo dpkg -i puppetlabs-release-pc1-xenial.deb
 sudo apt-get update
