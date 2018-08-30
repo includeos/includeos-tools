e68de9bf5e844eb2f09d5155975eb76a2e5247e1
taiyeba <staiyeba@gmail.com>
2018-08-30 16:33:05 +0200

    monitoring hosts: udpating agent config files

--- monitor_hosts_checkmk/install_checkmk_agent_debian.sh
+++ monitor_hosts_checkmk/install_checkmk_agent_debian.sh
@@ -14,5 +14,5 @@ logger "Checking port access: telnet-ing to port 6556"
 telnet localhost 6556
 echo "Reminder: Allow port 6556 for Igress"
 logger "Writing server IP to check_mk file"
-sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
+sudo sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
 echo "Installing check_mk_agent completed"
--- monitor_hosts_checkmk/install_checkmk_agent_rhel.sh
+++ monitor_hosts_checkmk/install_checkmk_agent_rhel.sh
@@ -7,12 +7,14 @@ export LC_ALL=en_US.UTF-8">>~/.bash_profile
 source ~/.bash_profile
 
 sudo yum install xinetd
+sudo yum install wget
 wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
 sudo yum install check_mk-agent-1.2.4p5-1.noarch.rpm
 sudo check_mk_agent
 logger "Checking port access: telnet-ing to port 6556"
+sudo yum install telnet
 telnet localhost 6556
 echo "Reminder: Allow port 6556 for Igress"
 logger "Writing server IP to check_mk file"
-sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
+sudo sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
 echo "Installing check_mk_agent completed"
