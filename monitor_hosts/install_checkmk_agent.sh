bd0b62ae7b14688304644539c01996488bd65bd1
taiyeba <staiyeba@gmail.com>
2018-08-30 09:56:26 +0200

    monitor_hosts: error fixes for check_mk agents

--- monitor_hosts/install_checkmk_agent.sh
+++ monitor_hosts/install_checkmk_agent.sh
@@ -8,9 +8,10 @@ source ~/.bash_profile
 
 sudo apt-get install xinetd
 wget http://mathias-kettner.de/download/check-mk-agent_1.2.4p5-2_all.deb
-dpkg -i check-mk-agent_1.2.4p5-2_all.deb
-check_mk_agent
+sudo dpkg -i check-mk-agent_1.2.4p5-2_all.deb
+sudo check_mk_agent
 logger "Checking port access: telnet-ing to port 6556"
-telnet host-ip 6556
+telnet localhost 6556
 logger "writing server IP to check_mk file"
-sed -i '42i    only_from = 192.168.0.8'
+sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
+echo "Installing check_mk_agent completed"
