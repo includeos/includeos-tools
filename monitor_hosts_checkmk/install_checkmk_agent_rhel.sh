b2fd05cd32cbb56c941c563f8b13f63dbe968f54
taiyeba <staiyeba@gmail.com>
2018-12-04 14:19:37 +0100

    monitoring_hosts: adding installation files for check_mk

---
+++ monitor_hosts_checkmk/install_checkmk_agent_rhel.sh
@@ -0,0 +1,20 @@
+# COnfiguring check_mk agents in hosts
+
+logger "Setting locale..."
+echo "export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8">>~/.bash_profile
+source ~/.bash_profile
+
+sudo yum install xinetd
+sudo yum install wget
+wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
+sudo yum install check_mk-agent-1.2.4p5-1.noarch.rpm
+sudo check_mk_agent
+logger "Checking port access: telnet-ing to port 6556"
+sudo yum install telnet
+telnet localhost 6556
+echo "Reminder: Allow port 6556 for Igress"
+logger "Writing server IP to check_mk file"
+sudo sed -i '42i    only_from = 192.168.0.8'  /etc/xinetd.d/check_mk
+echo "Installing check_mk_agent completed"
