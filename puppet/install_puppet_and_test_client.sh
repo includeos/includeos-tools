694ececcd5155279a8fe191a4634c7c14bd8afec
taiyeba <staiyeba@gmail.com>
2019-04-12 09:33:23 +0200

    puppet: adding manifest name as a parameter

--- puppet/install_puppet_and_test_client.sh
+++ puppet/install_puppet_and_test_client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+FILE="$1"
+
 wget https://apt.puppet.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt-get update
 
 sudo apt-get -y install puppet
 
-sudo puppet apply ~/includeos-tools/puppet/test_client.pp
+sudo puppet apply ~/includeos-tools/puppet/$FILE
