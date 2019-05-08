4a7cb910da0ddfdaf39354d875ca7b05dd4df49c
Taiyeba <staiyeba@gmail.com>
2019-05-08 09:21:59 +0200

    puppet: installs puppet-agent

--- puppet/install_puppet_and_test_client.sh
+++ puppet/install_puppet_and_test_client.sh
@@ -6,6 +6,6 @@ wget https://apt.puppet.com/puppet6-release-bionic.deb
 sudo dpkg -i puppet6-release-bionic.deb
 sudo apt-get update
 
-sudo apt-get -y install puppet
+sudo apt-get -y install puppet-agent
 
 sudo puppet apply ~/includeos-tools/puppet/$FILE
